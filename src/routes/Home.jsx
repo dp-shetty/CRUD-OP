ba4c587b6ca7422addcfccc8b0ce07a22421d5a1
dp-shetty <dps2k811@gmail.com>
2024-09-11 11:47:50 +0530

    updated homepage

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -5,10 +5,13 @@ import { useNavigate } from "react-router-dom";
 function Home() {
   const Navigate = useNavigate();
   const [userData, setUserData] = useState();
+  const [searchQuery, setSearchQuery] = useState("");
 
   const getUsersData = async () => {
     try {
-      const { data: users } = await axios.get("https://crud-backend-mocha.vercel.app/users");
+      const { data: users } = await axios.get(
+        "https://crud-backend-mocha.vercel.app/users"
+      );
       setUserData(users);
     } catch (error) {}
   };
@@ -38,6 +41,12 @@ function Home() {
     getUsersData();
   }, []);
 
+  const filteredData = userData?.filter(({ email, role, name }) => {
+    name.toLowerCase().includes(searchQuery.toLowerCase()) ||
+      email.toLowerCase().includes(searchQuery.toLowerCase()) ||
+      role.toLowerCase().includes(searchQuery.toLowerCase());
+  });
+
   const tableStyle = {
     border: "1px solid black",
     borderCollapse: "collapse",
@@ -50,8 +59,15 @@ function Home() {
       <header className="flex justify-center h-14 items-center font-sans text-2xl bg-blue-400 text-white w-full">
         WELCOME TO REACT CRUD OPERATIONS
       </header>
+      <input
+        type="text"
+        className="p-3 border border-gray-400 rounded w-1/2 m-auto outline-none"
+        placeholder="Search by name or email"
+        value={searchQuery}
+        onChange={(e) => setSearchQuery(e.target.value)}
+      />
       <main className="w-11/12 m-auto flex justify-center">
-        <table style={{ ...tableStyle, width: "100%" }} className="">
+        <table style={{ ...tableStyle, width: "100%" }}>
           <thead>
             <tr>
               <th style={{ ...tableStyle }}>ID</th>
@@ -62,43 +78,48 @@ function Home() {
             </tr>
           </thead>
           <tbody>
-            {userData &&
-              userData.map(({ name, email, role, id }) => {
-                return (
-                  <tr key={id}>
-                    <td style={{ ...tableStyle }}>{id}</td>
-                    <td style={{ ...tableStyle }}>{name}</td>
-                    <td style={{ ...tableStyle }}>{email}</td>
-                    <td style={{ ...tableStyle }}>{role}</td>
-                    <td style={{ ...tableStyle }} className="flex gap-5">
-                      <button
-                        className="w-1/3 bg-blue-400 rounded-2xl h-10 text-white"
-                        onClick={() => {
-                          handleView(id);
-                        }}
-                      >
-                        VIEW
-                      </button>
-                      <button
-                        className="w-1/3 bg-orange-500 rounded-2xl h-10 text-white"
-                        onClick={() => {
-                          handleUpdate(id);
-                        }}
-                      >
-                        UPDATE
-                      </button>
-                      <button
-                        className="w-1/3 bg-red-700 rounded-2xl h-10 text-white"
-                        onClick={() => {
-                          handleDelete(id);
-                        }}
-                      >
-                        DELETE
-                      </button>
-                    </td>
-                  </tr>
-                );
-              })}
+            {filteredData?.length > 0 ? (
+              filteredData.map(({ name, email, role, id }) => (
+                <tr key={id}>
+                  <td style={{ ...tableStyle }}>{id}</td>
+                  <td style={{ ...tableStyle }}>{name}</td>
+                  <td style={{ ...tableStyle }}>{email}</td>
+                  <td style={{ ...tableStyle }}>{role}</td>
+                  <td style={{ ...tableStyle }} className="flex gap-5">
+                    <button
+                      className="w-1/3 bg-blue-400 rounded-2xl h-10 text-white"
+                      onClick={() => {
+                        handleView(id);
+                      }}
+                    >
+                      VIEW
+                    </button>
+                    <button
+                      className="w-1/3 bg-orange-500 rounded-2xl h-10 text-white"
+                      onClick={() => {
+                        handleUpdate(id);
+                      }}
+                    >
+                      UPDATE
+                    </button>
+                    <button
+                      className="w-1/3 bg-red-700 rounded-2xl h-10 text-white"
+                      onClick={() => {
+                        handleDelete(id);
+                      }}
+                    >
+                      DELETE
+                    </button>
+                  </td>
+                </tr>
+              ))
+            ) : (
+              <tr>
+                <td colSpan="5" style={{ ...tableStyle }}>
+                  No users found
+                </td>
+              </tr>
+            )}
           </tbody>
           <tfoot>
             <tr>
